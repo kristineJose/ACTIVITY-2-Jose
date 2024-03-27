91fb21560270807a8e18bdfba25566eb667f3d4f
Kristine Jose <kristin.jose24@gmail.com>
2024-03-27 12:13:02 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ACTIVITY-5-JOSE',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
